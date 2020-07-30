ebf4d82958893fc2afdd82a94b7093cb9d975056
Charlin Agramonte <char0394@gmail.com>
2020-07-30 13:45:46 -0400

    Adding sample project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ShowHidePasswordTrigger
+# Show  HidePassword Trigger in Xamarin Forms
+
+<img width="200" src="sample.gif" />
+
+Blog post: https://xamgirl.com
+
---
+++ ShowHidePasswordTrigger/ShowPasswordTriggerAction.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShowHidePasswordTrigger
+{
+    public class ShowPasswordTriggerAction : TriggerAction<ImageButton>, INotifyPropertyChanged
+    {
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        bool _hidePassword = true;
+
+        public bool HidePassword
+        {
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+            get => _hidePassword;
+        }
+
+        protected override void Invoke(ImageButton sender)
+        {
+            sender.Source = HidePassword ? ShowIcon : HideIcon;
+            HidePassword = !HidePassword;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
